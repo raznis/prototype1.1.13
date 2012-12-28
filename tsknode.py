de5e5197e207c883ceb8c008ab85d3d6ea436e31
polak <adipola@post.bgu.ac.il>
2012-12-28 13:05:35 +0200

    delete the getProb table and coopy it go general node

--- tsknode.py
+++ tsknode.py
@@ -5,9 +5,7 @@
 """
 
 from Node import node
-from distributions.computed import Computed
-from distributions.normal import Normal
-from distributions.uniform import Uniform
+
 
 
 class TskNode (node):
@@ -57,103 +55,5 @@ class TskNode (node):
     
 #######################-----Adi changes(23/12/2012)-----####################
 
-    #table is the name of the table needed- attribute
-    def createDistTable(self,table):
-        string = self.getAttrib(str(table))
-        
-        table =[]        
-        if string != None:
-            table = self._parseString(string)
-        newDistTable =[]
-        #loop over the table- range (0,table len-1)- specifying the step value as 2
-        for index in range(len(table)):
-            #computed dist   
-            if (table[index][0] == 'C'):
-                newDistTable.append(self._createComputedDist(table[index]))
-            #normal dist
-            if(str(table[index][0]) =='N'):
-                newDistTable.append(self._createNormalDist(table[index]))
-            #discrete dist
-            if(table[index][0] == 'D'):
-                pass
-            #iniform dist- create new instance and 
-            if(table[index][0] == 'U'):
-                x=self._createUniformDist(table[index])
-#                x.printMe()
-                newDistTable.append(x)
-                
-        return newDistTable
-            
-    #create computed distribution
-    def _createComputedDist(self,Sinput):
-        ans =self._getDictOfNumPairFromString(Sinput)
-        return Computed(ans)        
-    #create normal distribution
-    def _createNormalDist(self,Sinput):
-      ans = self._getTwoNumFromString(Sinput)
-      return Normal(ans[0],ans[1])
-       
-    #create uniform distribution  
-    def _createUniformDist(self,Sinput):
-       ans = self._getTwoNumFromString(Sinput)
-       return Uniform(ans[0],ans[1])
-    
-    def _createDiscreteDist(self,string):
-        pass
-     
-
-    #input- string "num,num" output: tauple [num,num]
-    # we use this func to divide two numbers for distribution parmeters value
-    #can only work for two numbers in the string
-    def _getTwoNumFromString(self,Sinput):
-      stringNumA = ""
-      stringNumB = ""
-      nextNum = False
-      
-      #loop over the string
-      for index in range(0, len(Sinput)):  
-          #check if the Sinput[index] is a number or "." - for float num.
-          if (Sinput[index].isdigit() or Sinput[index]=='.' ) == True and (nextNum == False):
-              stringNumA += str( Sinput[index] )
-              continue
-          if(str(Sinput[index]) ==','):
-              nextNum= True
-              continue
-          if (Sinput[index].isdigit() or Sinput[index]=='.') == True and (nextNum == True):
-              stringNumB+= str(Sinput[index] ) 
-              continue
-              
-      #return a tauple of two str that represent float number
-      return [str(stringNumA),str(stringNumB)]
-      
-      
-      
-    # Sinput should look like this - C[123,123],[123,1231],[54,23] 
-    #input- the string above, output: disctionary of key and value
-    #we use this func to create the map/dictionary for computed distribution
-    def _getDictOfNumPairFromString(self,Sinput):
-        openBracket = False
-        stringPair=""
-        #start pairList as empty dictionary
-        PairList = {}
-        #iter from index=0 to strint- Sinput size
-        for index in range(0,len(Sinput)):
-            if Sinput[index] == '[' and openBracket == False :
-                openBracket = True
-                continue
-            if Sinput[index] == ']' and openBracket == True:
-                #call getTwoNumFromString func with stringPair and appand to the PairList- to get a tauple[num,num]
-                pair = self._getTwoNumFromString(stringPair)
-                PairList[str(pair[0])]= str(pair[1])
-                #update open bracket to close                
-                openBracket = False
-                #init the stringPair
-                stringPair = ""
-                continue
-            if openBracket == True :
-                stringPair += Sinput[index]
-                continue
-        #return distionry  
-        return PairList
-            
+   
             
